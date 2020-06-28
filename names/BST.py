c11502bf4e4a071b2c24cbfe75dbadf0cda7f5a0
Darren <dangus1924@gmail.com>
2020-06-28 19:38:36 -0400

    fixed error with insert right with the class

--- names/BST.py
+++ names/BST.py
@@ -1,50 +1,57 @@
-class Bst:
-    def __init__(self, value):
+class Bstree:                              
+    def __init__(self, value):                  
         self.value = value
-        self.left = None
-        self.right = None 
+        self.left = None                             
+        self.right = None                            
 
     def __repr__(self):
-        return (f"Value: {self.value}, Left: {self.left}, Right: {self.right}")
+        return (f"Value= {self.value}, Left: {self.left}, Right: {self.right}")
 
+    # Insert the given value into the tree
     def insert(self, value):
-        if self.value > value:
-            if self.laeft == None:
-                self.left = Bst(value)
-            else: 
-                self.left.isert(value)
+        # SIMPLIFY:
+        if  self.value > value:                       
+            if self.left == None:                     
+                 self.left = Bstree(value)  
+            else:
+                self.left.insert(value)        
 
-        if self.value <= value:
+        if  self.value <= value:
             if self.right == None:
-                self.right.Bst(value)
+                self.right = Bstree(value)
             else:
-                self.right.insert(value)
+                self.right.insert(value) 
 
+    # Return True if the tree contains the value
+    # False if it does not
     def contains(self, target):
-        if target == self.value:
-            return True
-        elif target > self.value:
-            if self.right != None:
-                return self.right.contains(target)
-            else:
-                return False
+        # base case
+        if target == self.value:                    
+            return True                             
+        elif target > self.value:                    
+            if self.right != None:                  
+                return self.right.contains(target)   
+            else:                                   
+                    return False                    
+            
+        elif target < self.value:                   
+            if self.left != None:                   
+                return self.left.contains(target)    
+            else:                                   
+                return False 
 
-        elif target < self.value:
-            if self.left != None:
-                return self.left.contains(target)
-            else:
-                return False
-
+  
     def get_max(self):
-        if self.right != None:
-            return self.right.get_max()
+       
+        if self.right != None:                        
+            return self.right.get_max()               
         else:
-            return self.value
-
-
-    def for_each(self, fn):
-        fn(self.value)
-        if self.right:
-            self.right.for_each(fn)
-        if self.left:
-            self.left.for_each(fn)
+            return self.value                         
+   
+    def for_each(self, cb):                           
+        
+        cb(self.value)                                 
+        if self.right:                                 
+            self.right.for_each(cb)
+        if self.left:                                  
+            self.left.for_each(cb)
