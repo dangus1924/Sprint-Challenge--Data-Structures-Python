6566b886db173f9227a829fa89d41bc8833b57c3
Darren <dangus1924@gmail.com>
2020-06-28 19:19:23 -0400

    created a table for names, created basic insert function, not tested

--- names/BST.py
+++ names/BST.py
@@ -2,4 +2,20 @@ class Bst:
     def __init__(self, value):
         self.value = value
         self.left = None
-        self.right = None      
+        self.right = None 
+
+    def __repr__(self):
+        return (f"Value: {self.value}, Left: {self.left}, Right: {self.right}")
+
+    def insert(self, value):
+        if self.value > value:
+            if self.laeft == None:
+                self.left = Bst(value)
+            else: 
+                self.left.isert(value)
+
+        if self.value <= value:
+            if self.right == None:
+                self.right.Bst(value)
+            else:
+                self.right.insert(value)
