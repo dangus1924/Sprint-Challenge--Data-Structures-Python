51c6c48d6f4904bde76db593aaa34657c559b53f
Darren <dangus1924@gmail.com>
2020-06-28 21:25:26 -0400

    completed ring buffer all test are now passing

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,4 +1,4 @@
-from Linked_list import DoublyLinkedList
+from linked_list import DoublyLinkedList
 
 class RingBuffer:
     def __init__(self, capacity):
@@ -7,7 +7,25 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        if self.storage.length < self.capacity:
+            self.storage.add_to_tail(item)
+        elif self.storage.length == self.capacity:
+            if not self.storage.tail.next:
+                self.storage.tail.next = self.storage.head
+                self.current = self.storage.head
+            self.current.value = item
+            self.current = self.current.next
 
     def get(self):
-        pass
+        list_buffer_contents = []
+        next_node = self.storage.head.next
+        list_buffer_contents.append(self.storage.head.value)
+        for _ in range(self.capacity - 1):
+            if next_node is None:
+                pass
+            else:
+                list_buffer_contents.append(next_node.value)
+                next_node = next_node.next
+            
+
+        return list_buffer_contents
